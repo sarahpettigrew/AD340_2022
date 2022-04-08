880e8ceb6baa0ad68b569b330952c68a201e970e
Sarah Pettigrew <sarahepettigrew@outlook.com>
2022-04-08 00:02:09 -0700

    Added image to app with ordered list of fun facts and changed color scheme

--- app/src/main/java/com/pettigrew/ad340_22/MainActivity.kt
+++ app/src/main/java/com/pettigrew/ad340_22/MainActivity.kt
@@ -2,12 +2,16 @@ package com.pettigrew.ad340_22
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.text)
+        textView.setText(R.string.bulleted_list)
         Log.d("MainActivity", "Hello World")
     }
 }
