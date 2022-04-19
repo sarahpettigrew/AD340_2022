25479b5e65cfb6585d7b1ab33e98b2e749a28515
SarahPettigrew <sarahepettigrew@outlook.com>
2022-04-18 20:30:52 -0700

    Added 2 x 2 grid and started to work on adding toasts

--- app/src/main/java/com/pettigrew/ad340_22/MainActivity.kt
+++ app/src/main/java/com/pettigrew/ad340_22/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.pettigrew.ad340_22
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,10 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        From Assignment 1:
 //        val textView = findViewById<TextView>(R.id.text)
 //        textView.setText(R.string.bulleted_list)
+//        Log.d("MainActivity", "Hello World")
 
-
-        Log.d("MainActivity", "Hello World")
+    }
+    fun sendMessage(view: View) {
+        // Do something in response to button click
+        val text = "test"
+        val duration = Toast.LENGTH_SHORT
+        Toast.makeText(applicationContext, text, duration).show()
     }
 }
