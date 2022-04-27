e3232f4fbba8a7f079cfd2dd252241fb3bf62f01
SarahPettigrew <sarahepettigrew@outlook.com>
2022-04-26 19:14:27 -0700

    Movie Activity added and Movie button links to Movie Activity instead of toast

--- app/src/main/java/com/pettigrew/ad340_22/MainActivity.kt
+++ app/src/main/java/com/pettigrew/ad340_22/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.pettigrew.ad340_22
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -17,6 +19,14 @@ class MainActivity : AppCompatActivity() {
 //        Log.d("MainActivity", "Hello World")
 
     }
+
+    fun buttonSelected(button : View){
+        if(button.id == R.id.movieButton){
+            val intent =  Intent(this, MovieActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     fun sendMessage(button: View) {
         // Do something in response to button click
         val b = button as Button
